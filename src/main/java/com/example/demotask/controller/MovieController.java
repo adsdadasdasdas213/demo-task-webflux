4e611d89a2c8769980bd979cf905e5a08591c04c
Bahriddin <bakhr1dd1n17@gmail.com>
2021-12-01 16:30:04 +0500

    appLog file logback file Logging history

--- src/main/java/com/example/demotask/controller/MovieController.java
+++ src/main/java/com/example/demotask/controller/MovieController.java
@@ -2,7 +2,8 @@ package com.example.demotask.controller;
 
 import com.example.demotask.model.Movie;
 import com.example.demotask.repository.MovieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -11,36 +12,55 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/movies")
 public class MovieController {
 
-    @Autowired
+    // most popular SLF4j
+
+    // logger.trace("A TRACE Message");
+    // logger.debug("A DEBUG Message");
+    // logger.info("An INFO Message");
+    // logger.warn("A WARN Message");
+    // logger.error("An ERROR Message");
+
+    Logger logger = LoggerFactory.getLogger(MovieController.class);
+
+    final
     MovieRepository movieRepository;
+
+    public MovieController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
 
     @GetMapping
     public Flux<Movie> getALl() {
+        logger.info("INFO Find all");
         return movieRepository.findAll();
     }
 
     @GetMapping("/{id}")
     public Mono<Movie> getOne(@PathVariable Integer id) {
+        logger.info("INFO get One");
         return movieRepository.findById(id);
     }
 
     @PostMapping
     public Mono<Movie> add(@RequestBody Movie movie) {
+        logger.info("INFO Add");
         return movieRepository.save(movie);
     }
 
     @PutMapping("/{id}")
     public Mono<Movie> edit(@PathVariable Integer id, @RequestBody Movie movie) {
+        logger.info("INFO Edit");
         return movieRepository.findById(id).map((c) -> {
             c.setName(movie.getName());
             c.setCountry(movie.getCountry());
             c.setDescription(movie.getDescription());
             return c;
-        }).flatMap(c -> movieRepository.save(c));
+        }).flatMap(movieRepository::save);
     }
 
     @DeleteMapping("/{id}")
     public Mono<Void> delete(@PathVariable Integer id) {
+        logger.warn("Warn Delete");
         return movieRepository.deleteById(id);
     }
 }
